5b2aae280fc629eca538e96aa3f57e9c558513fb
ElenaKhodenkova <novavselena1980@gmail.com>
2024-10-02 14:43:54 +0300

    Footer2

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,6 +1,74 @@
+import { Typography, Grid, Divider, Box, img } from '@material-ui/core';
+import styles from './Footer.module.css';
 
-export default function Footer() {
+export default function Footer(): JSX.Element {
   return (
-    <div>Footer</div>
-  )
+    <div className={styles.footer}>
+      <Divider />
+      <div className={styles.footerContent}>
+        <Grid container spacing={2} >
+          <Grid item xs={12} md={3}>
+            <Box
+              display="flex" justifyContent="space-between" alignItems="center" px={3}>
+              <Box mt={2}>
+                <img
+                  src="/tk3.png"
+                  style={{ maxWidth: '60%', height: 'auto' }}
+                />
+              </Box>
+            </Box>
+            <Box mt={2}>
+              <Typography variant="body2" align="center" className={styles.copyright}>
+                Copyright ©️2009-2024 TECHNO-KITCHEN.
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <Typography variant="h5" align="center" style={{ color: 'white' }}>
+              Модели
+            </Typography>
+            <Box mt={2} >
+              {['Игровые компьютеры', 'Рабочие станции', 'Каталог'].map(
+                (item) => (
+                  <Typography
+                    key={item} align="center" style={{ color: 'white' }}>
+                    {item}
+                  </Typography>
+                )
+              )}
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <Typography variant="h5" align="center" style={{ color: 'white' }}>
+              Категории
+            </Typography>
+            <Box mt={2}>
+              {[
+                'Процессоры', 'Видеокарты', 'Материнские платы', 'Оперативная память', 'SSD и HDD', 'Системы охлаждения',
+              ].map((item) => (
+                <Typography
+                  key={item} align="center" style={{ color: 'white' }}>
+                  {item}
+                </Typography>
+              ))}
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <Typography variant="h5" align="center" style={{ color: 'white' }}>
+              Наши контакты
+            </Typography>
+            <Box mt={2}>
+              <Typography align="center" style={{ color: 'white' }}>
+                8 (800) 000 00-00
+              </Typography>
+              <Typography align="center" style={{ color: 'white' }}>
+                г. Волково, ул. Фантазийная, д.123
+              </Typography>
+            </Box>
+          </Grid>
+        </Grid>
+      </div>
+      <Divider />
+    </div>
+  );
 }
