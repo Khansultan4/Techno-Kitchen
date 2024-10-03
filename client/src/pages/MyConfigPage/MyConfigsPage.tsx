f88b652e0134b77f2e59aff84d887bf632ce2747
Khansultan4 <sultan.bisultanov.04@mail.ru>
2024-10-03 21:00:58 +0400

    infobutton

--- client/src/pages/MyConfigPage/MyConfigsPage.tsx
+++ client/src/pages/MyConfigPage/MyConfigsPage.tsx
@@ -6,6 +6,7 @@ import StarsReadOnly from '../../components/StarsReadOnly/StarsReadOnly';
 import axiosInstance from '../../../axiosInstance';
 import { IBuild } from '../../types/types';
 import { initBuild } from '../../types/initStates';
+import InfoButton from '../../components/InfoButton/InfoButton';
 export default function MyConfigsPage(): JSX.Element {
 
   const [entries, setEntries] = useState <IBuild[]>([initBuild]);
@@ -21,24 +22,13 @@ export default function MyConfigsPage(): JSX.Element {
   }, []);
   console.log('124',typeof entries[0]?.updatedAt);
     
-  function createData(
-        photo: string,
-        name: string,
-        price: string,
-        update: string,
-        rating: JSX.Element,
-        moore: string,
-      ) {
-        return { photo, name, price, update, rating, moore };
-      }
+
       // const rows = entries
       // const avergeScore = entries.reduce((acc,el) => acc + el, 0)/entries.length
       console.log('2121', entries);
       
-      const rows = entries.map((el) => 
-        createData(el.image,el.title, '268000 Р', el.updatedAt, StarsReadOnly({value: el.Ratings.reduce((acc,val) => acc + val.score, 0)/el.Ratings.length}), '...'),
-      )
 
+// console.log('123214', InfoButton);
 
 
     return (
@@ -56,19 +46,19 @@ export default function MyConfigsPage(): JSX.Element {
            </TableRow>
          </TableHead>
          <TableBody>
-           {rows.map((row) => (
+           {entries.map((el) => (
              <TableRow
-               key={row.name}
+               key={el.title}
                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
              >
                <TableCell >
-                <img src={row.photo} height='30px'></img>
-                {row.name}
+                <img src={el.image} height='30px'></img>
+                {el.title}
                 </TableCell>
-               <TableCell>{row.price}</TableCell>
-               <TableCell>{row.update}</TableCell>
-               <TableCell>{row.rating}</TableCell>
-               <TableCell>{row.moore}</TableCell>
+               <TableCell>268000 Р</TableCell>
+               <TableCell>{el.updatedAt}</TableCell>
+               <TableCell><StarsReadOnly value={el.Ratings.reduce((acc,val) => acc + val.score, 0)/el.Ratings.length} /></TableCell>
+               <TableCell> <InfoButton id={el.id}/></TableCell>
              </TableRow>
            ))}
          </TableBody>
